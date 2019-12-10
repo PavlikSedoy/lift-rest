1e0ea57f36f3a8c4a3f413dc2b27c5347f603134
PavelSedoy <pavliksedoy@gmail.com>
2019-12-10 05:53:17 +0200

    Before adding gallery

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    svg4everybody({});
+    // BEGIN HOME GALLERY TABS
+    $('.gallery-tabs__link').click( function(e) {
+        e.preventDefault();
+
+        // Begin tab changing
+        $('.gallery-tabs__link').each( function() {
+            $(this).parent().removeClass('active');
+        });
+
+        $(this).parent().addClass('active');
+        // End tab changing
+
+        // Begin tab content changing
+        var clickedTabName =  $(this).data('tab');
+
+        $('.tabs-content__item').each( function() {
+            $(this).slideUp();
+        });
+
+        $('#' + clickedTabName).slideDown();
+        // End tab content changing
+    });
+    // END HOME GALLERY TABS
 });
